0a782925198e196af5551955bcc3ac95465e67f4
unknown <ezeldobro@live.com.ar>
2018-10-15 22:23:59 -0300

    Initial Commit

---
+++ Circunferencia/Circunferencia/Form1.Designer.vb
@@ -0,0 +1,204 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RadioTxt = New System.Windows.Forms.TextBox()
+        Me.Diam = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Circun = New System.Windows.Forms.Label()
+        Me.Area = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(147, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 31)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Radio"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'RadioTxt
+        '
+        Me.RadioTxt.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioTxt.Location = New System.Drawing.Point(251, 30)
+        Me.RadioTxt.Multiline = True
+        Me.RadioTxt.Name = "RadioTxt"
+        Me.RadioTxt.Size = New System.Drawing.Size(117, 32)
+        Me.RadioTxt.TabIndex = 1
+        Me.RadioTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Diam
+        '
+        Me.Diam.Font = New System.Drawing.Font("Modern No. 20", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Diam.Location = New System.Drawing.Point(201, 108)
+        Me.Diam.Name = "Diam"
+        Me.Diam.Size = New System.Drawing.Size(120, 44)
+        Me.Diam.TabIndex = 3
+        Me.Diam.Text = "-"
+        Me.Diam.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(46, 108)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(123, 44)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Diametro"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(46, 197)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(123, 40)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Circunferencia"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.SystemColors.Highlight
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(46, 285)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(123, 41)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Area"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(236, 197)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(150, 44)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "-"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Circun
+        '
+        Me.Circun.Font = New System.Drawing.Font("Modern No. 20", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Circun.Location = New System.Drawing.Point(201, 197)
+        Me.Circun.Name = "Circun"
+        Me.Circun.Size = New System.Drawing.Size(120, 44)
+        Me.Circun.TabIndex = 5
+        Me.Circun.Text = "-"
+        Me.Circun.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Area
+        '
+        Me.Area.Font = New System.Drawing.Font("Modern No. 20", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Area.Location = New System.Drawing.Point(201, 283)
+        Me.Area.Name = "Area"
+        Me.Area.Size = New System.Drawing.Size(120, 44)
+        Me.Area.TabIndex = 7
+        Me.Area.Text = "-"
+        Me.Area.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(217, 384)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(122, 47)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "Limpiar"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(437, 377)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(120, 60)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "Salir"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(365, 99)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(214, 227)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox3.TabIndex = 15
+        Me.PictureBox3.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(594, 451)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Area)
+        Me.Controls.Add(Me.Circun)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Diam)
+        Me.Controls.Add(Me.RadioTxt)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Circunferencia"
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents RadioTxt As TextBox
+    Friend WithEvents Diam As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Circun As Label
+    Friend WithEvents Area As Label
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents PictureBox3 As PictureBox
+End Class
---
+++ Circunferencia/Circunferencia/Form1.vb
@@ -0,0 +1,74 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim R, D As Double
+
+        If IsNumeric(RadioTxt.Text) = True Then
+            R = Convert.ToDouble(RadioTxt.Text)
+            If CDbl(RadioTxt.Text) > 0 And CDbl(RadioTxt.Text) < 100 Then
+                D = 2 * R
+                Diam.Text = (D).ToString
+            Else
+                MessageBox.Show("Error, rango excedido")
+                RadioTxt.Clear()
+            End If
+        Else
+            MessageBox.Show("Erro, ingrese solo numeros")
+            RadioTxt.Clear()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim R, Cir, Pi As Double
+
+        If IsNumeric(RadioTxt.Text) = True Then
+            R = Convert.ToDouble(RadioTxt.Text)
+            If CDbl(RadioTxt.Text) > 0 And CDbl(RadioTxt.Text) < 100 Then
+                Pi = 3.14159265
+                Cir = 2 * Pi * R
+                Circun.Text = (Cir).ToString
+            Else
+                MessageBox.Show("Error, rango excedido")
+                RadioTxt.Clear()
+            End If
+        Else
+            MessageBox.Show("Error, debe ingresar numeros")
+            RadioTxt.Clear()
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim R, A, Pi As Double
+
+        If IsNumeric(RadioTxt.Text) = True Then
+            R = Convert.ToDouble(RadioTxt.Text)
+            If CDbl(RadioTxt.Text) > 0 And CDbl(RadioTxt.Text) < 100 Then
+                Pi = 3.14159265
+                A = Pi * R ^ 2
+                Area.Text = (A).ToString
+            Else
+                MessageBox.Show("Error, rango excedido")
+                RadioTxt.Clear()
+            End If
+        Else
+            MessageBox.Show("Error, debe ingresar numeros")
+            RadioTxt.Clear()
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Me.Close()
+    End Sub
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        If MessageBox.Show("¿Desea salir del programa?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.No Then
+            e.Cancel = True
+
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        RadioTxt.Clear()
+        Diam.Text = ""
+        Circun.Text = ""
+        Area.Text = ""
+    End Sub
+End Class
